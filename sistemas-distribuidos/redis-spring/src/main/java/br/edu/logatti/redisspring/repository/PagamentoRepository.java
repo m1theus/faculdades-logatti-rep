1c03c240106b73e57800acf381ba34da5d517ae8
Matheus Martins da Silva <cin_masilva@uolinc.com>
2021-05-28 20:52:05 -0300

    Revert "add redis spring  app"
    This reverts commit 4c7422ce966377b367863e0ec21836417ae22891.

--- sistemas-distribuidos/redis-spring/src/main/java/br/edu/logatti/redisspring/repository/PagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.logatti.redisspring.repository;
-
-import br.edu.logatti.redisspring.entity.Pagamento;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PagamentoRepository extends MongoRepository<Pagamento, String> {
-}
