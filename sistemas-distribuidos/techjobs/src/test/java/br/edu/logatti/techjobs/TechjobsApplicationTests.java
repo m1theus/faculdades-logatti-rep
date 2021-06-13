37020a49fdff17d84bb260cb017a6f944821ec65
Matheus Martins <matheus.math27@hotmail.com>
2021-06-13 02:34:21 -0300

    add tech jobs project

---
+++ sistemas-distribuidos/techjobs/src/test/java/br/edu/logatti/techjobs/TechjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.logatti.techjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
